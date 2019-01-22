8936f196c9980c4ac03e97f22834be60887426b6
sam ghoreishi <sam20gh@gmail.com>
2019-01-22 11:43:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -31,3 +31,8 @@ def last_element_with_array_methods(last_country)
   @south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
   last_country = @south_east_asia.[-1]
 end
+
+def length_of_array(length)
+  @programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
+  length = @programming_languages.length
+end

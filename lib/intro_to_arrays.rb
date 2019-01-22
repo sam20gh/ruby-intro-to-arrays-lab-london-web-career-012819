c806132f3f3409673ca74beb893266a7d4440d2d
sam ghoreishi <sam20gh@gmail.com>
2019-01-22 11:41:32 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -17,4 +17,7 @@ def third_element(my_third_element)
   my_third_element = @taylor_swift[2]
 end
 
-def last_element
+def last_element(my_last_element)
+  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  my_last_element = @taylor_swift.last
+end

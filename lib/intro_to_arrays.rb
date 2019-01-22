7b906a623e99b113dbf9e3088bd330c3d9f1d360
sam ghoreishi <sam20gh@gmail.com>
2019-01-22 11:42:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -21,3 +21,8 @@ def last_element(my_last_element)
   @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   my_last_element = @taylor_swift.last
 end
+
+def last_element(my_last_element)
+  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  my_last_element = @taylor_swift[-1]
+end

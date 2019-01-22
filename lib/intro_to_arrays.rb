3c5bce543efdb0d9a9388d57da918fb18acb3acb
sam ghoreishi <sam20gh@gmail.com>
2019-01-22 11:38:51 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,14 @@
+def instantiate_new_array
+  instantiate_new_array = []
+  @my_new_array = instantiate_new_array
+end
+
+def array_with_two_elements
+  array_with_two_elements = ["one","two"]
+  @my_two_array = array_with_two_elements
+end
+
+def first_element(my_first_element)
+   @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+   my_first_element = @taylor_swift.first
+end
